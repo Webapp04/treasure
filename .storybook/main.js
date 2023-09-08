68581b7acb246f7011d0584d574f0170504efcee
Webapp04 <expertwebapp04@gmail.com>
2023-09-08 13:01:08 +0530

    Build

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../src/**/*.stories.jsx"],
+  addons: ["@storybook/addon-controls", "@storybook/preset-create-react-app"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+};
